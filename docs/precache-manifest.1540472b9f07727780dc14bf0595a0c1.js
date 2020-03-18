27a3313dd7af7bcacc9c26745b97d092952cf0c0
sutej-pal <sutejpal@hotmail.com>
2020-03-19 00:01:37 +0530

    added scss

---
+++ docs/precache-manifest.1540472b9f07727780dc14bf0595a0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5b2a318765590b02f329df86b1ff31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6847b8b752e133bbf527",
+    "url": "/static/css/main.651109a9.chunk.css"
+  },
+  {
+    "revision": "0a896da5a9e8cd9a0793",
+    "url": "/static/js/2.cdbd3410.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cdbd3410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6847b8b752e133bbf527",
+    "url": "/static/js/main.cfa1739f.chunk.js"
+  },
+  {
+    "revision": "4dbed8ab34d4d5755ddd",
+    "url": "/static/js/runtime-main.906ebcfb.js"
+  }
+]);
